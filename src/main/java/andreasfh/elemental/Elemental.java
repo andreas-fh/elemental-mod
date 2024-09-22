7b6325b7ae5ded5f29389acc9f07a1e151da163c
BuildTools <unconfigured@null.spigotmc.org>
2024-09-22 19:35:14 +0200

    Initial commit

---
+++ src/main/java/andreasfh/elemental/Elemental.java
@@ -0,0 +1,16 @@
+package andreasfh.elemental;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Elemental implements ModInitializer {
+	public static final String MOD_ID = "elemental";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/andreasfh/elemental/ElementalClient.java
@@ -0,0 +1,11 @@
+package andreasfh.elemental;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElementalClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
