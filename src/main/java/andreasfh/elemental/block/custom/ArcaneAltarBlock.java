1e4f0a07cdcb7fe58aa268446c9d531ce19694d0
BuildTools <unconfigured@null.spigotmc.org>
2024-09-25 22:59:05 +0200

    Fixed Arcane Altar model and VoxelShape.

--- src/main/java/andreasfh/elemental/block/custom/ArcaneAltarBlock.java
+++ src/main/java/andreasfh/elemental/block/custom/ArcaneAltarBlock.java
@@ -4,14 +4,21 @@ import andreasfh.elemental.item.ModItems;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+
+import java.util.stream.Stream;
 
 public class ArcaneAltarBlock extends Block {
 
@@ -93,7 +100,7 @@ public class ArcaneAltarBlock extends Block {
             double angle = 2 * Math.PI * (particleIndex % particlesPerRevolution) / particlesPerRevolution;
             double offsetX = Math.cos(angle) * radius;
             double offsetZ = Math.sin(angle) * radius;
-            double currentY = pos.getY() + 1.1 + (particleIndex * heightStep);
+            double currentY = pos.getY() + 0.9 + (particleIndex * heightStep);
 
             world.addParticle(ParticleTypes.END_ROD,
                     centerX + offsetX,
@@ -102,4 +109,47 @@ public class ArcaneAltarBlock extends Block {
                     0.0, 0.0, 0.0);  // No velocity for static particles
         }
     }
+
+    @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+    private static final VoxelShape SHAPE = Stream.of(
+            Block.createCuboidShape(1, 0, 1, 15, 2, 15),
+            Block.createCuboidShape(4, 2, 4, 12, 9, 12),
+            Block.createCuboidShape(2, 8, 0, 14, 11, 2),
+            Block.createCuboidShape(2, 8, 14, 14, 11, 16),
+            Block.createCuboidShape(14, 8, 2, 16, 11, 14),
+            Block.createCuboidShape(0, 8, 2, 2, 11, 14),
+            Block.createCuboidShape(2, 11, 14, 14, 12, 16),
+            Block.createCuboidShape(0, 11, 2, 2, 12, 14),
+            Block.createCuboidShape(14, 11, 2, 16, 12, 14),
+            Block.createCuboidShape(2, 11, 0, 14, 12, 2),
+            Block.createCuboidShape(0, 10, 0, 2, 15, 2),
+            Block.createCuboidShape(0, 10, 14, 2, 15, 16),
+            Block.createCuboidShape(0, 15, 14, 2, 16, 16),
+            Block.createCuboidShape(14, 15, 14, 16, 16, 16),
+            Block.createCuboidShape(14, 15, 0, 16, 16, 2),
+            Block.createCuboidShape(0, 15, 0, 2, 16, 2),
+            Block.createCuboidShape(14, 10, 14, 16, 15, 16),
+            Block.createCuboidShape(14, 10, 0, 16, 15, 2),
+            Block.createCuboidShape(0, 12, 2, 2, 14, 4),
+            Block.createCuboidShape(0, 12, 12, 2, 14, 14),
+            Block.createCuboidShape(14, 12, 12, 16, 14, 14),
+            Block.createCuboidShape(14, 12, 2, 16, 14, 4),
+            Block.createCuboidShape(12, 12, 0, 14, 14, 2),
+            Block.createCuboidShape(2, 12, 0, 4, 14, 2),
+            Block.createCuboidShape(2, 12, 14, 4, 14, 16),
+            Block.createCuboidShape(12, 12, 14, 14, 14, 16),
+            Block.createCuboidShape(2, 11, 2, 6, 12, 4),
+            Block.createCuboidShape(10, 11, 2, 14, 12, 4),
+            Block.createCuboidShape(10, 11, 12, 14, 12, 14),
+            Block.createCuboidShape(2, 11, 12, 6, 12, 14),
+            Block.createCuboidShape(12, 11, 4, 14, 12, 6),
+            Block.createCuboidShape(12, 11, 10, 14, 12, 12),
+            Block.createCuboidShape(2, 11, 10, 4, 12, 12),
+            Block.createCuboidShape(2, 11, 4, 4, 12, 6),
+            Block.createCuboidShape(2, 8, 2, 14, 11, 14)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
 }
